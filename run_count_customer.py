cd0be276f3672c5cfb516d17fb6661a8cb90d9eb
Brocpis <pisit1205.work@gmail.com>
2022-10-15 00:28:48 +0700

    update autorun

--- run_count_customer.py
+++ run_count_customer.py
@@ -20,7 +20,7 @@ sleep(10)
 # run ros core
 Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'source /opt/ros/noetic/setup.bash && roscore'])
 
-cur_path = "/home/ilm/index_count_customer/flow"
+cur_path = "/home/ilm/Desktop/count_customer/flow"
 # run camera 1-3 flow
 flow1_3 = cur_path + "/1_3.flow"
 Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && source /opt/ros/noetic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && export LD_LIBRARY_PATH=$(find /usr/lib -iname nvidia-* -type d 2>&1 | sed "{:q;N;s/\\n/:/g;t q}"):${LD_LIBRARY_PATH} && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64 && while true; do rosrun cira_core cira_core_run --geometry +0+100 _file:='+f'{flow1_3} _hide_toolbar:=true; sleep 2s; done ;$SHELL'])
